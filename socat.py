a4c0f5d73ac58fb2598fe634ce0ca615f66429af
Shaleen Rawat <116910466+smcoder5@users.noreply.github.com>
2024-04-03 17:04:19 +0530

    Update socat.py

--- socat.py
+++ socat.py
@@ -75,7 +75,8 @@ def folium_map(aa):
             m=folium.Map(tiles=tiles,attr=attr,min_lat=-90,max_lat=90,min_lon=-180,max_lon=180,max_bounds=True,zoom_start=3,max_zoom = 9,min_zoom = 2,location=[0,0])
             for i in range(df.shape[0]):
                 folium.CircleMarker(location=(a[' LAT'][i],a[' LON'][i]),radius=2.5, color=color(df[dd1[aa]+"_"+dd2[v3]][i]), fill_color =color(df[dd1[aa]+"_"+dd2[v3]][i]),popup=a[df[dd1[aa]+"_"+dd2[v3]][i]][i], fill_opacity=1).add_to(m)
-            st_data = st_folium(m,width=1750)
+            st_data = st_folium(m,width=1750) 
+    s.write(f)
 if __name__=="__main__":
     s.set_page_config(page_title='Sea Water Dataset',layout='wide')
     s.title("  Surface Ocean CO₂ Atlas ")
