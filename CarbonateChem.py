b7bc6aab4869448389725d274f9edd3db3bab44c
Shaleen Rawat <116910466+smcoder5@users.noreply.github.com>
2024-04-24 20:51:23 +0530

    Update CarbonateChem.py

--- CarbonateChem.py
+++ CarbonateChem.py
@@ -45,7 +45,7 @@ if __name__=="__main__":
     with co[1]:
         co[1].metric("",value=hco3(pK1(temp,salt),co2,pH))
     with co[2]:
-        col[2].metric("",value=co3(pK1(temp,salt),pK2(temp,salt),co2,pH))
+        co[2].metric("",value=co3(pK1(temp,salt),pK2(temp,salt),co2,pH))
     del co
     co=s.columns(2)
     with co[0]:
